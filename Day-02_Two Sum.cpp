845a9dd52ba021ef24dac9f5219121331c0a832d
Ishika Goyal <isbika2002@gmail.com>
2024-07-28 17:03:29 +0530

    Day-02_Two Sum.cpp

--- Day-02_Two Sum.cpp
+++ Day-02_Two Sum.cpp
@@ -1,7 +1,58 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        //Brute Force Approach - O(n^2)
+        
+        /* vector<int> twoSum(int n, vector<int> &arr, int target) {
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+    return { -1, -1};
+}*/
 
+        //Better Approach - Using Hashing - O(n), SC - O(n)
+
+        /*
+            vector<int> twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return { -1, -1};
+}*/
+
+        //Optimal Approach - Using two pointer - O(n) + O(n*logn), SC- O(1)
+
+        /*
+            string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
+        }
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "NO";
+}*/
+
+        //TC - O(n*logn)
+        
        vector<pair<int, int>> num_pairs;
         for (int i = 0; i < nums.size(); ++i) {
             num_pairs.push_back({nums[i], i});
@@ -32,4 +83,3 @@ public:
 };
 
 
-//Use hashing for O(n) TC
